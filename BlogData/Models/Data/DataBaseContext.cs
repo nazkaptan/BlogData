92f411e59bb110ed6c89d4672d12aba40c7e2322
pkapt <pkapt@NAZ>
2023-01-09 11:19:14 +0300

    Add project files.

---
+++ BlogData/Models/Data/DataBaseContext.cs
@@ -0,0 +1,25 @@
+﻿using BlogData.Models.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogData.Models.Data
+{
+    public class DataBaseContext : DbContext
+
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Article> Articles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().HasData(new User(1, "Naz", "1234"));
+            modelBuilder.Entity<Article>().Property(x => x.CreatedTime).HasDefaultValueSql("getutcdate()");
+
+        }
+
+    }
+}
---
+++ BlogData/Models/Entity/Article.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogData.Models.Entity
+{
+    [Table("Articles")]
+    public class Article
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public string ArticlePicture { get; set; }
+        public DateTime CreatedTime { get; set; }=DateTime.Now;
+
+        [ForeignKey("Author")]
+        public int AuthorId { get; set; }
+        public virtual User Author { get; set; }
+    }
+}
